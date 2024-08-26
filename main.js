cdcb6ff4ee600997f1ceccd29f3005f6eb4e0e3e
LoveVadel <loveosagie@hotmail.com>
2024-08-26 13:51:30 +0100

    Created the subtract function
    this function takes two numbers and subtract the second from the first

--- main.js
+++ main.js
@@ -2,3 +2,10 @@
 function add(num1, num2){
     return num1 + num2;
 }
+
+//Create a function that takes in two numbers and subtracts one from the other.
+function subtract(num1, num2){
+    return num1 - num2
+}
+
+console.log(subtract(7, 8))

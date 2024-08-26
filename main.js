b3453efb776e90a753a267978131af02d64ae753
LoveVadel <loveosagie@hotmail.com>
2024-08-26 14:03:24 +0100

    Created the multiply and divide functions

--- main.js
+++ main.js
@@ -5,7 +5,16 @@ function add(num1, num2){
 
 //Create a function that takes in two numbers and subtracts one from the other.
 function subtract(num1, num2){
-    return num1 - num2
+    return num1 - num2;
 }
 
-console.log(subtract(7, 8))
+// create a function that takes in numbers and multiply them to return a final number.
+function multiply(num1, num2){
+    return num1 * num2;
+}
+
+//Create a function that takes in two numbers and divide one with the other to return a final number.
+function divide(num1, num2){
+    return num1 / num2;
+}
+
